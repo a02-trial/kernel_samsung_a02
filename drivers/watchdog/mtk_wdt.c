d798ee0c2fbf3992259ed7af83e2102ba8d94de2
a02-trial <cipin23@gmail.com>
2026-04-24 02:15:37 +0700

    Update mtk_wdt.c
    Signed-off-by: a02-trial <cipin23@gmail.com>

--- drivers/watchdog/mtk_wdt.c
+++ drivers/watchdog/mtk_wdt.c
@@ -228,7 +228,11 @@ static int mtk_reset_handler(struct notifier_block *this, unsigned long mode,
 		mtk_misc_mark_fast();
 		#endif
 	} else {
-		//do nothing
+		/* Normal reboot - write reboot reason so bootloader auto-boots
+		 * without waiting for power button press.
+		 * 0x77665501 = standard MTK normal reboot magic number
+		 */
+		writel(0x77665501, wdt_base + WDT_NONRST_REG);
 	}
 
 	if (!arm_pm_restart) {
